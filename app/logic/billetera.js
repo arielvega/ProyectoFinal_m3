1cd9a0989adfa1a9cc962d706d592149a5013faa
Luis Ariel Vega Soliz <luisvega@uagrm.edu.bo>
2019-09-02 19:10:08 -0400

    agregando metodos de registrar ingresos y salidas, con sus respectivas pruebas

--- app/logic/billetera.js
+++ app/logic/billetera.js
@@ -1,9 +1,33 @@
 module.exports = {
     Billetera: function(montoInicial){
-        this.monto = montoInicial;
+        var _saldo;
+        _saldo = function(_montoInicial) {
+            if (_montoInicial<0){
+                throw new Error("Monto Inicial no debe ser menor a 0");
+            }
+            return _montoInicial
+        }(montoInicial);
+
         this.consultarSaldo = function () {
-            return this.monto;
-        }
+            return _saldo;
+        };
+        this.registrarIngreso = function(monto){
+            if (monto<0){
+                throw new Error("Monto no debe ser menor a 0");
+            }
+            _saldo += monto;
+            return _saldo;
+        };
+        this.registrarSalida = function(monto){
+            if (monto<0){
+                throw new Error("Monto no debe ser menor a 0");
+            }
+            if (monto >_saldo){
+                throw new Error("Saldo insuficiente en la billetera");
+            }
+            _saldo -= monto;
+            return _saldo;
+        };
     }
 }
 
--- test/TestBilletera.js
+++ test/TestBilletera.js
@@ -1,23 +1,81 @@
 var assert = require("chai").assert;
+var expect    = require("chai").expect;
 var modulo = require("../app/logic/billetera.js");
 
 
 describe("TestBilletera",function(){
+
     describe("Test al constructor", function() {
+
         it('Al crear la billetera con un monto 0, este monto se refleja en el saldo', function () {
-            billetera = new modulo.Billetera(0);
+            montoInicial = 0;
+            billetera = new modulo.Billetera(montoInicial);
             saldo = billetera.consultarSaldo();
             saldoEsperado = 0;
             assert.equal(saldo, saldoEsperado);
         });
 
         it('Al crear la billetera con un monto > 0, este monto se refleja en el saldo', function () {
-            billetera = new modulo.Billetera(50);
+            montoInicial = 50;
+            billetera = new modulo.Billetera(montoInicial);
             saldo = billetera.consultarSaldo();
             saldoEsperado = 50;
             assert.equal(saldo, saldoEsperado);
         });
+
+        it('Al crear la billetera con un monto < 0, este debe lanzar la excepcion "Monto Inicial no debe ser menor a 0"', function () {
+           expect(() => new modulo.Billetera(-1)).to.throw(Error, /Monto Inicial no debe ser menor a 0/);
+        });
     });
 
+    describe("Test al metodo 'registrar ingreso'", function() {
+
+        it('Si la billetera tiene un monto inicial = 0, entonces al ejecutar el metodo "registrar ingreso" con un monto mayor a cero, este monto se refleja como nuevo monto de la billetera', function () {
+            montoInicial = 0;
+            ingreso = 25;
+            billetera = new modulo.Billetera(montoInicial);
+            billetera.registrarIngreso(ingreso);
+            saldo = billetera.consultarSaldo();
+            saldoEsperado = ingreso;
+            assert.equal(saldo, saldoEsperado);
+        });
+
+        it('Si la billetera tiene un monto inicial = 0, entonces al ejecutar el metodo "registrar ingreso" con un monto igual a cero, la billetera continua en saldo 0', function () {
+            montoInicial = 0;
+            ingreso = 0;
+            billetera = new modulo.Billetera(montoInicial);
+            billetera.registrarIngreso(ingreso);
+            saldo = billetera.consultarSaldo();
+            saldoEsperado = 0;
+            assert.equal(saldo, saldoEsperado);
+        });
+
+        it('Si la billetera tiene un monto inicial > 0, entonces al ejecutar el metodo "registrar ingreso" con un monto mayor a cero, este monto se aumenta al saldo inicial de la billetera', function () {
+            montoInicial = 10;
+            ingreso = 16;
+            billetera = new modulo.Billetera(montoInicial);
+            billetera.registrarIngreso(ingreso);
+            saldo = billetera.consultarSaldo();
+            saldoEsperado = montoInicial + ingreso;
+            assert.equal(saldo, saldoEsperado);
+        });
+
+        it('Si la billetera tiene un monto inicial > 0, entonces al ejecutar el metodo "registrar ingreso" con un monto igual a cero, la billetera continua con el saldo que tenia inicialmente', function () {
+            montoInicial = 150;
+            ingreso = 0;
+            billetera = new modulo.Billetera(montoInicial);
+            billetera.registrarIngreso(ingreso);
+            saldo = billetera.consultarSaldo();
+            saldoEsperado = montoInicial;
+            assert.equal(saldo, saldoEsperado);
+        });
+
+        it('Si se ejecuta el metodo "registrar ingreso" con un monto menor a cero, se lanza una excepcion "Monto no debe ser menor a 0"', function () {
+            montoInicial = 150;
+            ingreso = 0;
+            billetera = new modulo.Billetera(montoInicial);
+            expect(() => billetera.registrarIngreso(-1)).to.throw(Error, /Monto no debe ser menor a 0/);
+        });
+    });
 });
 
